71320075a2914026d221119faf7e3c0c5b6b7dd2
ZephirFX <zephirfx.pro@gmail.com>
2025-07-18 00:47:46 +0100

    Support optional vcpkg installation / Allow external VCPKG and improve error handling for existing vcpkg paths

--- build.sh
+++ build.sh
@@ -38,21 +38,21 @@ parse_args()
 # Parse the version from a command line argument
 parse_version()
 {
-    VERSION="$( cut -d ':' -f 2- <<< "$s" )"
+    VERSION="$( cut -d ':' -f 2- <<< "$1" )"
     log_info "Version specified by user: $VERSION"
 }
 
 # Parse the installation dir from a command line argument
 parse_install_dir()
 {
-    INSTALLDIR="$( cut -d ':' -f 2- <<< "$s" )"
+    INSTALLDIR="$( cut -d ':' -f 2- <<< "$1" )"
     log_info "Install directory specified by user: $INSTALLDIR"
 }
 
-# Parse the python path from a command line argument
+# Parse the houdini path from a command line argument
 parse_houdini_path()
 {
-    HOUPATH="$( cut -d ':' -f 2- <<< "$s" )"
+    HOUPATH="$( cut -d ':' -f 2- <<< "$1" )"
     log_info "Houdini path specified by user: $HOUPATH"
 }
 
@@ -82,34 +82,28 @@ source_vcpkg_bootstrap()
 
 # Entry point
 
-if [[ $HELP -eq 1 ]]; then
-    echo VQVDB build script for Linux
-    echo.
-    echo Usage:
-    echo   - build ^<arg1^> ^<arg2^> ...
-    echo     - args:
-    echo       --debug: builds in debug mode, defaults to release
-    echo       --tests: runs CMake tests, if any
-    echo       --clean: removes the old build directory
-    echo       --install: runs CMake installation, if any
-    echo       --installdir:^<install_directory_path^>: path to the install directory, default to ./install
-    echo       --version:^<version^>: specifies the version, defaults to $VERSION
-    echo       --houdinipath:^<houdini_path^>: specifies the path to HOUDINI
-    echo       --help/-h: displays this message and exits
-    exit 0
-fi
-
 log_info "Building VQVDB"
 
-if [[ ! -d "vcpkg" ]]; then
-    log_info "Vcpkg can't be found, cloning and preparing it"
-    git clone https://github.com/microsoft/vcpkg.git
-    cd vcpkg
-    source_vcpkg_bootstrap
-    cd ..
+if [[ -n "${VCPKG_ROOT}" ]]; then
+    log_info "Using existing VCPKG_ROOT from environment: $VCPKG_ROOT"
+    if [[ ! -x "$VCPKG_ROOT/vcpkg" ]]; then
+        log_error "VCPKG_ROOT is set but vcpkg executable not found in it."
+        exit 1
+    fi
+else
+    if [[ -d "vcpkg" ]]; then
+        export VCPKG_ROOT=$PWD/vcpkg
+        log_info "Using local vcpkg directory: $VCPKG_ROOT"
+    else
+        log_info "Vcpkg can't be found, cloning and preparing it"
+        git clone https://github.com/microsoft/vcpkg.git
+        cd vcpkg
+        source_vcpkg_bootstrap
+        cd ..
+        export VCPKG_ROOT=$PWD/vcpkg
+    fi
 fi
 
-export VCPKG_ROOT=$PWD/vcpkg
 export CMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake
 
 if [[ $PATH != *$VCPKG_ROOT* ]]; then
@@ -123,7 +117,19 @@ do
 done
 
 if [[ $HELP -eq 1 ]]; then
-    show_help
+    echo "VQVDB build script for Linux"
+    echo ""
+    echo "Usage:"
+    echo "  - build <arg1> <arg2> ..."
+    echo "    - args:"
+    echo "      --debug: builds in debug mode, defaults to release"
+    echo "      --tests: runs CMake tests, if any"
+    echo "      --clean: removes the old build directory"
+    echo "      --install: runs CMake installation, if any"
+    echo "      --installdir:<install_directory_path>: path to the install directory, default to ./install"
+    echo "      --version:<version>: specifies the version, defaults to $VERSION"
+    echo "      --houdinipath:<houdini_path>: specifies the path to HOUDINI"
+    echo "      --help/-h: displays this message and exits"
     exit 0
 fi
 
@@ -146,9 +152,9 @@ if [[ -d "build" && $REMOVEOLDDIR -eq 1 ]]; then
     rm -rf build
 fi
 
-if [[ -d "install" && $REMOVEOLDDIR -eq 1 ]]; then
+if [[ -d "$INSTALLDIR" && $REMOVEOLDDIR -eq 1 ]]; then
     log_info "Removing old install directory"
-    rm -rf install
+    rm -rf "$INSTALLDIR"
 fi
 
 cmake -S . -B build -DRUN_TESTS=$RUNTESTS -DCMAKE_EXPORT_COMPILE_COMMANDS=$EXPORTCOMPILECOMMANDS -DCMAKE_BUILD_TYPE=$BUILDTYPE -DVERSION=$VERSION
